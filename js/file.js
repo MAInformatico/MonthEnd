1e8c6bdb172c4a787580bc14ae0027e0c9495fca
MAInformatico <magutierrezsistemas@gmail.com>
2019-02-26 20:45:20 +0100

    adding styles

--- js/file.js
+++ js/file.js
@@ -1,1 +1,14 @@
 //This file contains all the functions created for month end
+
+	<script>
+		var app = angular.module('calculate', []);
+		app.controller('ctrl', function($scope) {
+		    $scope.salary= 0;
+		    $scope.rentb= 0;
+		    $scope.comub= 0;
+		    $scope.elecb= 0;
+		    $scope.watb= 0;
+		    $scope.intb= 0;
+		    $scope.gasb= 0;
+		});
+	</script>
