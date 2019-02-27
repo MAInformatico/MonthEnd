26c2f9125a2a1633269efe98f901aad01ff22159
MAInformatico <magutierrezsistemas@gmail.com>
2019-02-27 19:26:14 +0100

    fixing code

--- js/file.js
+++ js/file.js
@@ -1,4 +1,4 @@
-//This file contains all the functions created for month end
+//Functions in AngularJS for MonthEnd
 	
 		var app = angular.module('calculate', []);
 		app.controller('ctrl', function($scope) {
